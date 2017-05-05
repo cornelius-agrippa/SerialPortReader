4ea1f8d50b875e8d530b736b3645739850b82f53
cornelius-agrippa <bitbybit@sapo.pt>
2017-05-05 14:43:03 +0100

    First import of the original code;

---
+++ Serial.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace SerialReader
+{
+    partial class Serial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Serial));
+            this.labelPort = new System.Windows.Forms.Label();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxMessageIn = new System.Windows.Forms.TextBox();
+            this.textBoxMessageOut = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshSerialPortsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearMessagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonReload = new System.Windows.Forms.Button();
+            this.statusStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelPort
+            // 
+            resources.ApplyResources(this.labelPort, "labelPort");
+            this.labelPort.Name = "labelPort";
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBoxPort, "comboBoxPort");
+            this.comboBoxPort.Name = "comboBoxPort";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusStripLabel});
+            resources.ApplyResources(this.statusStrip, "statusStrip");
+            this.statusStrip.Name = "statusStrip";
+            // 
+            // statusStripLabel
+            // 
+            this.statusStripLabel.Name = "statusStripLabel";
+            resources.ApplyResources(this.statusStripLabel, "statusStripLabel");
+            // 
+            // buttonStart
+            // 
+            resources.ApplyResources(this.buttonStart, "buttonStart");
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            resources.ApplyResources(this.buttonStop, "buttonStop");
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxMessageIn
+            // 
+            resources.ApplyResources(this.textBoxMessageIn, "textBoxMessageIn");
+            this.textBoxMessageIn.Name = "textBoxMessageIn";
+            this.textBoxMessageIn.ReadOnly = true;
+            // 
+            // textBoxMessageOut
+            // 
+            resources.ApplyResources(this.textBoxMessageOut, "textBoxMessageOut");
+            this.textBoxMessageOut.Name = "textBoxMessageOut";
+            this.textBoxMessageOut.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxMessageOut_KeyDown);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.menuStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip, "menuStrip");
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.saveAsToolStripMenuItem, "saveAsToolStripMenuItem");
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.Form1_FormClosing);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.refreshSerialPortsToolStripMenuItem,
+            this.clearMessagesToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            resources.ApplyResources(this.optionsToolStripMenuItem, "optionsToolStripMenuItem");
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            resources.ApplyResources(this.startToolStripMenuItem, "startToolStripMenuItem");
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            resources.ApplyResources(this.stopToolStripMenuItem, "stopToolStripMenuItem");
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // refreshSerialPortsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.refreshSerialPortsToolStripMenuItem, "refreshSerialPortsToolStripMenuItem");
+            this.refreshSerialPortsToolStripMenuItem.Name = "refreshSerialPortsToolStripMenuItem";
+            this.refreshSerialPortsToolStripMenuItem.Click += new System.EventHandler(this.buttonReload_Click);
+            // 
+            // clearMessagesToolStripMenuItem
+            // 
+            this.clearMessagesToolStripMenuItem.Name = "clearMessagesToolStripMenuItem";
+            resources.ApplyResources(this.clearMessagesToolStripMenuItem, "clearMessagesToolStripMenuItem");
+            this.clearMessagesToolStripMenuItem.Click += new System.EventHandler(this.clearMessagesToolStripMenuItem_Click);
+            // 
+            // buttonSave
+            // 
+            resources.ApplyResources(this.buttonSave, "buttonSave");
+            this.buttonSave.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonSend
+            // 
+            resources.ApplyResources(this.buttonSend, "buttonSend");
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonReload
+            // 
+            resources.ApplyResources(this.buttonReload, "buttonReload");
+            this.buttonReload.Name = "buttonReload";
+            this.buttonReload.UseVisualStyleBackColor = true;
+            this.buttonReload.Click += new System.EventHandler(this.buttonReload_Click);
+            // 
+            // Serial
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessageIn);
+            this.Controls.Add(this.textBoxMessageOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttonReload);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(this.labelPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "Serial";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel statusStripLabel;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonReload;
+        private System.Windows.Forms.TextBox textBoxMessageIn;
+        private System.Windows.Forms.TextBox textBoxMessageOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshSerialPortsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearMessagesToolStripMenuItem;
+    }
+}
+
---
+++ Serial.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace SerialReader
+{
+    public partial class Serial : Form
+    {
+        private SerialPort MySerial;
+        private bool DeviceConnected;
+
+        public Serial()
+        {
+            InitializeComponent();
+
+            MySerial = new SerialPort();
+            RefreshSerialPorts();    
+
+            textBoxMessageIn.ScrollBars = ScrollBars.Vertical;
+        }
+
+        ~Serial()
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            if (MySerial.IsOpen)
+                MySerial.Close();
+        }
+
+        private void RefreshSerialPorts()
+        {
+            // Serial port detection
+            string[] ports = SerialPort.GetPortNames();
+
+            comboBoxPort.Items.Clear();
+            comboBoxPort.Text = "(none)";
+
+            // Check if there's serial port readys for communication
+            if (ports.Length <= 1) {
+                buttonStart.Enabled = false;
+                buttonStop.Enabled = false;
+
+                MessageBox.Show
+                (
+                    "No open Serial Ports detected. Check your device's connection and/or drivers.",
+                    "No Serial Ports", MessageBoxButtons.OK, MessageBoxIcon.Error
+                );
+                DeviceConnected = false;
+            }
+
+            Console.Write("Serial ports ready: ");
+
+            foreach (string port in ports)
+            {
+                Console.Write("{0} ", port);
+                comboBoxPort.Items.Add(port);
+            }
+            Console.WriteLine();
+            comboBoxPort.SelectedIndex = 0;
+
+            MySerial.BaudRate = 9600;
+            MySerial.DtrEnable = true;
+
+            DeviceConnected = true;
+
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        // Method for capturing any message received through the serial port
+        private void MySerial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            String rxString = MySerial.ReadLine();
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), rxString);
+        }
+
+        private delegate void LineReceivedEvent(string POT);
+
+        private void LineReceived(string POT)
+        {
+            //  Split values
+            //  String[] DataReceived = POT.Split(',');
+
+            textBoxMessageIn.AppendText(POT + "\n");
+        }
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (DeviceConnected)
+            {
+                MySerial.PortName = comboBoxPort.Text;
+                MySerial.Open();
+                
+                // Add event for capturing serial messages
+                MySerial.DataReceived += MySerial_DataReceived;
+            }
+
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+            buttonReload.Enabled = false;
+
+            startToolStripMenuItem.Enabled = false;
+            stopToolStripMenuItem.Enabled = true;
+            refreshSerialPortsToolStripMenuItem.Enabled = false;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (DeviceConnected)
+            {
+                if (MySerial.IsOpen)
+                    MySerial.Close();
+
+                MySerial.DataReceived -= MySerial_DataReceived; 
+            }
+
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+
+            buttonReload.Enabled = true;
+
+            startToolStripMenuItem.Enabled = true;
+            stopToolStripMenuItem.Enabled = false;
+            refreshSerialPortsToolStripMenuItem.Enabled = true;
+        }
+
+        private void buttonReload_Click(object sender, EventArgs e)
+        {
+            RefreshSerialPorts();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (DeviceConnected)
+            {
+                MySerial.Write(textBoxMessageOut.Text);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            saveFileDialog.FileName = unixTimestamp.ToString();
+            saveFileDialog.Filter = "Text File (*.txt)|*.txt|Comma Separated Values (*.csv)|*.csv|Data File (*.dat)|*.dat|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName, false, System.Text.Encoding.UTF8))
+                {
+                    sw.WriteLine(textBoxMessageIn.Text);
+                }
+            }
+        }
+
+        private void clearMessagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBoxMessageIn.Clear();
+        }
+
+        private void textBoxMessageOut_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonSend_Click(sender, e);
+            }
+        }
+     }
+}
